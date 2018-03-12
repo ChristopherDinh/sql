344edc9cad11407493aa9460e7e03f7791cf97f2
ChristopherDinh <chrisvdinh@gmail.com>
2018-03-12 13:36:41 -0400

    updating loyalty

--- membership/loyalty.sql
+++ membership/loyalty.sql
@@ -4,22 +4,34 @@ USE loyalty;
 
 CREATE TABLE Membership (
 ID INT PRIMARY KEY AUTO_INCREMENT,
-Number INT NOT NULL,
+Number INT NOT NULL Unique,
 FirstName VARCHAR(25) NOT NULL,
 LastName VARCHAR(25) NOT NULL,
 Address VARCHAR(50) NOT NULL,
 City VARCHAR(30) NOT NULL,
 State VARCHAR(25) NOT NULL,
 Zip VARCHAR(25) NOT NULL,
-Telephone VARCHAR(10) NOT NULL,
-Email VARCHAR(50) NULL,
-Employee INT NOT NULL
+Telephone VARCHAR(15) NOT NULL,
+Email VARCHAR(50),
+Employee INT Default 0 NOT NULL
 );
 
-CREATE TABLE Tranaction (
+CREATE TABLE PurchaseTranaction (
 ID INT PRIMARY KEY AUTO_INCREMENT,
 MembershipID INT NOT NULL,
-Purchases VARCHAR(100) NOT NULL,
+Number VARCHAR(100) NOT NULL,
 Date DATE NOT NULL,
-FOREIGN KEY (MembershipID) References Membership (ID)
+AmountTotal VARCHAR(50) NOT NULL,
+FOREIGN KEY (MembershipID) References Membership (ID),
+Constraint num_date Unique (Number, Date)
+);
+
+CREATE TABLE ReturnTranaction (
+ID INT PRIMARY KEY AUTO_INCREMENT,
+MembershipID INT NOT NULL,
+Number VARCHAR(100) NOT NULL,
+Date DATE NOT NULL,
+AmountTotal VARCHAR(50) NOT NULL,
+FOREIGN KEY (MembershipID) References Membership (ID),
+Constraint num_date Unique (Number, Date)
 );
